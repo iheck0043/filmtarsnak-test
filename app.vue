83a2c37939c1824045adccaf3fc0ae613dd8b717
iheck0043 <ahmad.aliizadeh.70@gmail.com>
2025-07-22 12:28:58 +0330

    feat: create nuxt project and add tailwind css setup

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="p-8 bg-gray-50 min-h-screen">
+    <NuxtRouteAnnouncer />
+    
+    <div class="max-w-4xl mx-auto">
+      <h1 class="text-2xl font-bold text-gray-800 mb-4">film tarsnak test project</h1>
+      
+     
+    </div>
+  </div>
+</template>
